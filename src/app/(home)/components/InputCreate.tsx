63aa1930f37b1efb07ebc3fe199e03895f1e5681
EsdrasCFF <ecff.dev@gmail.com>
2024-02-20 22:58:57 -0300

    fix: add useRef on inputs

--- src/app/(home)/components/InputCreate.tsx
+++ src/app/(home)/components/InputCreate.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useRef } from "react";
 
 export function InputCreate() {
+  
+  const name = useRef<HTMLInputElement>(null)
+  
   return (
     <form className="flex flex-col bg-secondary p-5 rounded-b-md gap-5">
 
-      <Input className="bg-gray-950 border-none" />
+      <Input className="bg-gray-950 border-none" placeholder="Digite seu nome" ref={name} />
       <Button className="text-black" > Entrar </Button>
     
     </form>

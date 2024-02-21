af3033fd9dbcd998d154a6b2c4c9e3ff4863d14b
EsdrasCFF <ecff.dev@gmail.com>
2024-02-20 21:49:48 -0300

    feat: create FormWrapper

---
+++ src/app/(home)/components/FormWrapper.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { InputJoin } from "./InputJoin"
+import { InputCreate } from "./InputCreate"
+
+export function FormWrapper() {
+  const [selectRoom, setSelectRoom] = useState<'join' | 'create'>('join')
+
+  function handleSelectRoom(room: 'join' | 'create') {
+
+    console.log(room)
+
+    setSelectRoom(room)
+  }
+
+  return (
+    <div className="w-full flex flex-col" >
+      <div className="grid grid-cols-2">
+        <Button 
+          onClick={() => handleSelectRoom('join')}
+          className= {`
+            w-full rounded-none rounded-t-md text-primary
+            ${selectRoom == 'join' ? 'bg-secondary hover:bg-secondary' : 'bg-black hover:bg-black' }
+          `}
+        >
+          Ingressar
+        </Button>
+        
+        <Button
+          onClick={() => handleSelectRoom('create')} 
+          className={`
+            "w-full rounded-none rounded-t-md"
+            ${selectRoom == 'create' ? 'bg-secondary hover:bg-secondary rounded-t-md' : 'bg-black hover:bg-black' }
+          `}
+        >
+          Nova reunião
+        </Button>
+      </div>
+      
+      {selectRoom == 'create' ? (
+        <InputCreate/>
+      ):(
+        <InputJoin />
+      )}
+    </div>
+  )
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,27 +2,14 @@ import { Header } from "@/components/Header";
 import { Container } from '@/components/Container'
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { FormWrapper } from "./components/FormWrapper";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col" >
       <Header />
-      <div className="max-w-[580px] mx-auto w-full h-full flex flex-1 items-center justify-center" >
-        
-        <div className="w-[400px] flex flex-col" >
-          <div className="flex">
-            <Button className="w-full bg-secondary rounded-none rounded-t-md text-primary" >Ingressar</Button>
-            <Button className="w-full bg-black rounded-none rounded-t-md" >Nova reunião</Button>
-          </div>
-          
-          <form className="flex flex-col bg-secondary p-5 rounded-b-md gap-5">
-
-            <Input className="bg-gray-950 border-none" />
-            <Input className="bg-gray-950 border-none" />
-            <Button className="text-black" > Entrar </Button>
-          
-          </form>
-        </div>
+      <div className="max-w-[500px] mx-auto w-full h-full flex flex-1 items-center justify-center" >
+        <FormWrapper />
       </div>
     </main>
   );

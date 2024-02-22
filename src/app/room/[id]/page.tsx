558a64b2cd5664e4979ef3546785ffabc788a935
EsdrasCFF <ecff.dev@gmail.com>
2024-02-22 16:58:11 -0300

    feat: shown and call camera to meet

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -3,12 +3,15 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { SocketContext } from "@/contexts/SocketContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { Chat } from "./components/Chat";
+import { initCamera } from "@/utils/initCamera";
 
 export default function Room({params}: {params: {id: string}}) {
 
   const {socket} = useContext(SocketContext)
+
+  const localStream = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
     socket?.on('connect', async () => {
@@ -18,6 +21,9 @@ export default function Room({params}: {params: {id: string}}) {
         roomId: params.id,
         socketId: socket.id
       })
+
+      const video = await initCamera()
+      localStream.current!.srcObject = video
     })
   }, [socket, params.id])
 
@@ -30,11 +36,11 @@ export default function Room({params}: {params: {id: string}}) {
           <div className="grid grid-cols-2 gap-5 h-full w-full" >
 
             <div className="relative bg-gray-950 w-full rounded-md h-full">
-              <video className="h-full w-full" ></video>
+              <video className="h-full w-full" autoPlay ref={localStream}></video>
               <span className="absolute bottom-1 left-1" >Esdras Castro</span>
             </div>
             <div className="relative bg-gray-950 w-full rounded-md h-full">
-              <video className="h-full w-full" ></video>
+              <video className="h-full w-full" autoPlay playsInline />
               <span className="absolute bottom-1 left-1" >Esdras Castro</span>
             </div>
             <div className="relative bg-gray-950 w-full rounded-md h-full">
